1d85413057f630f47adb708c9075a30872017f95
Oliver Fries <oliver.fries@iosb-ina.fraunhofer.de>
2024-05-29 15:29:43 +0200

    ExtendILangStringShortNameTypeIec61360: add tests and improve structure

---
+++ src/aaspe-common-tests/AasxCsharpLibrary/Extensions/ExtendILangStringShortNameTypeIec61360Test.cs
@@ -0,0 +1,136 @@
+﻿using AasCore.Aas3_0;
+using aaspe_common.AasxCsharpLibrary.Extensions;
+
+namespace aaspe_common_tests.AasxCsharpLibrary.Extensions;
+
+[TestSubject(typeof(ExtendILangStringShortNameTypeIec61360))]
+public class ExtendILangStringShortNameTypeIec61360Tests
+{
+    [Fact]
+    public void CreateLangStringShortNameType_ShouldCreateListWithSingleLangString()
+    {
+        // Arrange
+        const string language = "en";
+        const string text = "Example Text";
+
+        // Act
+        var result = ExtendILangStringShortNameTypeIec61360.CreateLangStringShortNameType(language, text);
+
+        // Assert
+        result.Should().HaveCount(1);
+        result.First().Language.Should().Be(language);
+        result.First().Text.Should().Be(text);
+    }
+
+    [Fact]
+    public void GetDefaultString_WithDefaultLanguageSet_ShouldReturnTextWithDefaultLanguage()
+    {
+        // Arrange
+        var langStrings = new List<ILangStringShortNameTypeIec61360>
+        {
+            new TestLangStringShortNameTypeIec61360("Hello", "en"),
+            new TestLangStringShortNameTypeIec61360("Bonjour", "fr")
+        };
+        const string defaultLang = "en";
+
+        // Act
+        var result = langStrings.GetDefaultString(defaultLang);
+
+        // Assert
+        result.Should().Be("Hello");
+    }
+
+    [Fact]
+    public void GetDefaultString_WithNoDefaultLanguage_ShouldReturnFirstText()
+    {
+        // Arrange
+        var langStrings = new List<ILangStringShortNameTypeIec61360>
+        {
+            new TestLangStringShortNameTypeIec61360("Hello", "en"),
+            new TestLangStringShortNameTypeIec61360("Bonjour", "fr")
+        };
+
+        // Act
+        var result = langStrings.GetDefaultString();
+
+        // Assert
+        result.Should().Be("Hello");
+    }
+
+    [Fact]
+    public void ConvertFromV20_WithSourceAndNonNullCount_ShouldCreateLangStringsFromSource()
+    {
+        // Arrange
+        var source = new AasxCompatibilityModels.AdminShellV20.LangStringSetIEC61360
+        {
+            new() {lang = "en", str = "Hello"},
+            new() {lang = "fr", str = "Bonjour"}
+        };
+
+        // Act
+        var result = new List<ILangStringShortNameTypeIec61360>().ConvertFromV20(source);
+
+        // Assert
+        result.Should().HaveCount(2);
+        result[0].Language.Should().Be("en");
+        result[0].Text.Should().Be("Hello");
+        result[1].Language.Should().Be("fr");
+        result[1].Text.Should().Be("Bonjour");
+    }
+
+    [Fact]
+    public void ConvertFromV20_WithEmptySource_ShouldCreateSingleLangStringWithDefaultLanguage()
+    {
+        // Arrange
+
+        // Act
+        var result = new List<ILangStringShortNameTypeIec61360>().ConvertFromV20(new AasxCompatibilityModels.AdminShellV20.LangStringSetIEC61360());
+
+        // Assert
+        result.Should().HaveCount(1);
+        result[0].Language.Should().Be("en");
+        result[0].Text.Should().Be("");
+    }
+}
+
+public class TestLangStringShortNameTypeIec61360 : ILangStringShortNameTypeIec61360
+{
+    public TestLangStringShortNameTypeIec61360(string text, string language)
+    {
+        Text = text;
+        Language = language;
+    }
+
+    public string Text { get; set; }
+    public string Language { get; set; }
+
+    public IEnumerable<IClass> DescendOnce()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<IClass> Descend()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Accept(Visitation.IVisitor visitor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Accept<TContext>(Visitation.IVisitorWithContext<TContext> visitor, TContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Transform<T>(Visitation.ITransformer<T> transformer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Transform<TContext, T>(Visitation.ITransformerWithContext<TContext, T> transformer, TContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/aaspe-common/AasxCsharpLibrary/Extensions/ExtendILangStringShortNameTypeIec61360.cs
+++ src/aaspe-common/AasxCsharpLibrary/Extensions/ExtendILangStringShortNameTypeIec61360.cs
@@ -7,44 +7,60 @@ This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
 This source code may use other Open Source software components (see LICENSE.txt).
 */
 
+using AasxCompatibilityModels;
+
 namespace aaspe_common.AasxCsharpLibrary.Extensions;
 
+/// <summary>
+/// Provides extension methods for ILangStringShortNameTypeIec61360.
+/// </summary>
 public static class ExtendILangStringShortNameTypeIec61360
 {
+    /// <summary>
+    /// Creates a list containing a single ILangStringShortNameTypeIec61360 instance with the specified language and text.
+    /// </summary>
+    /// <param name="language">The language of the short name.</param>
+    /// <param name="text">The text of the short name.</param>
+    /// <returns>A list containing a single ILangStringShortNameTypeIec61360 instance.</returns>
     public static List<ILangStringShortNameTypeIec61360> CreateLangStringShortNameType(string language, string text)
     {
-        return new List<ILangStringShortNameTypeIec61360> { new LangStringShortNameTypeIec61360(language, text) };
+        return new List<ILangStringShortNameTypeIec61360> {new LangStringShortNameTypeIec61360(language, text)};
     }
 
+    /// <summary>
+    /// Gets the default string from a list of short names using the specified default language.
+    /// </summary>
+    /// <param name="langStringSet">The list of short names.</param>
+    /// <param name="defaultLang">The default language.</param>
+    /// <returns>The default string.</returns>
     public static string GetDefaultString(this List<ILangStringShortNameTypeIec61360> langStringSet, string? defaultLang = null)
     {
         return ExtendLangString.GetDefaultStringGen(langStringSet, defaultLang);
     }
 
+    /// <summary>
+    /// Converts from AdminShellV20.LangStringSetIEC61360 to a list of ILangStringShortNameTypeIec61360.
+    /// </summary>
+    /// <param name="langStringShortNameType">The list of short names to be populated.</param>
+    /// <param name="src">The source lang string set.</param>
+    /// <returns>The converted list of short names.</returns>
     public static List<ILangStringShortNameTypeIec61360> ConvertFromV20(
-        this List<ILangStringShortNameTypeIec61360> lss,
-        AasxCompatibilityModels.AdminShellV20.LangStringSetIEC61360 src)
+        this List<ILangStringShortNameTypeIec61360> langStringShortNameType,
+        AdminShellV20.LangStringSetIEC61360 src)
     {
-        lss = new List<ILangStringShortNameTypeIec61360>();
-        if (src.Count != 0)
+        langStringShortNameType = new List<ILangStringShortNameTypeIec61360>();
+
+        if (src is {Count: > 0})
         {
-            foreach (var sourceLangString in src)
-            {
-                //Remove ? in the end added by AdminShellV20, to avoid verification error
-                var lang = sourceLangString.lang;
-                if (!string.IsNullOrEmpty(sourceLangString.lang) && sourceLangString.lang.EndsWith('?'))
-                {
-                    lang = sourceLangString.lang.Remove(sourceLangString.lang.Length - 1);
-                }
-                var langString = new LangStringShortNameTypeIec61360(lang, sourceLangString.str);
-                lss.Add(langString);
-            }
+            langStringShortNameType.AddRange(
+                (from sourceLangString in src let lang = sourceLangString.lang?.TrimEnd('?') select new LangStringShortNameTypeIec61360(lang ?? "en", sourceLangString.str)));
         }
         else
         {
-            //set default preferred name
-            lss.Add(new LangStringShortNameTypeIec61360("en", ""));
+            // Set default short name
+            langStringShortNameType.Add(new LangStringShortNameTypeIec61360("en", ""));
         }
-        return lss;
+
+        return langStringShortNameType;
     }
 }
