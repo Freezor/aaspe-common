764a1c18d45088ff1781be91f7fc4c8053dfe787
Oliver Fries <oliver.fries@iosb-ina.fraunhofer.de>
2024-05-29 15:41:29 +0200

    ExtendILangStringTextType: add tests and improve structure

---
+++ src/aaspe-common-tests/AasxCsharpLibrary/Extensions/ExtendILangStringTextTypeTest.cs
@@ -0,0 +1,132 @@
+﻿using AasCore.Aas3_0;
+using aaspe_common.AasxCsharpLibrary.Extensions;
+
+namespace aaspe_common_tests.AasxCsharpLibrary.Extensions;
+
+[TestSubject(typeof(ExtendILangStringTextType))]
+public class ExtendILangStringTextTypeTests
+{
+    [Fact]
+    public void GetDefaultString_WithDefaultLanguageSet_ShouldReturnTextWithDefaultLanguage()
+    {
+        // Arrange
+        var langStrings = new List<ILangStringTextType>
+        {
+            new TestLangStringTextType("Hello", "en"),
+            new TestLangStringTextType("Bonjour", "fr")
+        };
+        const string defaultLang = "en";
+
+        // Act
+        var result = langStrings.GetDefaultString(defaultLang);
+
+        // Assert
+        result.Should().Be("Hello");
+    }
+
+    [Fact]
+    public void GetDefaultString_WithNoDefaultLanguage_ShouldReturnFirstText()
+    {
+        // Arrange
+        var langStrings = new List<ILangStringTextType>
+        {
+            new TestLangStringTextType("Hello", "en"),
+            new TestLangStringTextType("Bonjour", "fr")
+        };
+
+        // Act
+        var result = langStrings.GetDefaultString();
+
+        // Assert
+        result.Should().Be("Hello");
+    }
+
+    [Fact]
+    public void ToStringExtended_WithFormat2_ShouldReturnTextWithLanguage()
+    {
+        // Arrange
+        var langString = new TestLangStringTextType("Hello", "en");
+        const int format = 2;
+
+        // Act
+        var result = langString.ToStringExtended(format);
+
+        // Assert
+        result.Should().Be("Hello@en");
+    }
+
+    [Fact]
+    public void ToStringExtended_WithDefaultFormat_ShouldReturnTextWithLanguageInBrackets()
+    {
+        // Arrange
+        var langString = new TestLangStringTextType("Hello", "en");
+        const int format = 1;
+
+        // Act
+        var result = langString.ToStringExtended(format);
+
+        // Assert
+        result.Should().Be("[en,Hello]");
+    }
+
+    [Fact]
+    public void ToStringExtended_WithDelimiter_ShouldReturnTextsWithLanguageAndDelimiter()
+    {
+        // Arrange
+        var langStrings = new List<ILangStringTextType>
+        {
+            new TestLangStringTextType("Hello", "en"),
+            new TestLangStringTextType("Bonjour", "fr")
+        };
+        const string delimiter = "|";
+
+        // Act
+        var result = langStrings.ToStringExtended(delimiter: delimiter);
+
+        // Assert
+        result.Should().Be("[en,Hello]|[fr,Bonjour]");
+    }
+
+}
+
+public class TestLangStringTextType : ILangStringTextType
+{
+    public TestLangStringTextType(string text, string language)
+    {
+        Text = text;
+        Language = language;
+    }
+
+    public string Text { get; set; }
+    public string Language { get; set; }
+
+    public IEnumerable<IClass> DescendOnce()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<IClass> Descend()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Accept(Visitation.IVisitor visitor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Accept<TContext>(Visitation.IVisitorWithContext<TContext> visitor, TContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Transform<T>(Visitation.ITransformer<T> transformer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Transform<TContext, T>(Visitation.ITransformerWithContext<TContext, T> transformer, TContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/aaspe-common/AasxCsharpLibrary/Extensions/ExtendILangStringTextType.cs
+++ src/aaspe-common/AasxCsharpLibrary/Extensions/ExtendILangStringTextType.cs
@@ -9,21 +9,43 @@ This source code may use other Open Source software components (see LICENSE.txt)
 
 namespace aaspe_common.AasxCsharpLibrary.Extensions;
 
+/// <summary>
+/// Provides extension methods for ILangStringTextType.
+/// </summary>
 public static class ExtendILangStringTextType
 {
+    /// <summary>
+    /// Gets the default string from a list of text types using the specified default language.
+    /// </summary>
+    /// <param name="langStringSet">The list of text types.</param>
+    /// <param name="defaultLang">The default language.</param>
+    /// <returns>The default string.</returns>
     public static string GetDefaultString(this List<ILangStringTextType> langStringSet, string? defaultLang = null)
     {
         return ExtendLangString.GetDefaultStringGen(langStringSet, defaultLang);
     }
 
+    /// <summary>
+    /// Converts an ILangStringTextType to a string with extended formatting.
+    /// </summary>
+    /// <param name="ls">The ILangStringTextType object.</param>
+    /// <param name="fmt">The format to use (1 or 2).</param>
+    /// <returns>The formatted string.</returns>
     public static string ToStringExtended(this ILangStringTextType ls, int fmt)
     {
         return fmt == 2 ? $"{ls.Text}@{ls.Language}" : $"[{ls.Language},{ls.Text}]";
     }
 
+    /// <summary>
+    /// Converts a collection of ILangStringTextType objects to a string with extended formatting.
+    /// </summary>
+    /// <param name="elems">The collection of ILangStringTextType objects.</param>
+    /// <param name="format">The format to use (1 or 2).</param>
+    /// <param name="delimiter">The delimiter to separate the items.</param>
+    /// <returns>The formatted string.</returns>
     public static string ToStringExtended(this IEnumerable<ILangStringTextType> elems,
         int format = 1, string delimiter = ",")
     {
-        return string.Join(delimiter, elems.Select((k) => k.ToStringExtended(format)));
+        return string.Join(delimiter, elems.Select(k => k.ToStringExtended(format)));
     }
 }
