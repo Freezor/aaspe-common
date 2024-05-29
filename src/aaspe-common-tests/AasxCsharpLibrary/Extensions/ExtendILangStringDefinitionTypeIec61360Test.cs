725501905fb83d70b9d7d633be56bb27a1b84ab0
Oliver Fries <oliver.fries@iosb-ina.fraunhofer.de>
2024-05-29 15:09:23 +0200

    add tests

---
+++ src/aaspe-common-tests/AasxCsharpLibrary/Extensions/ExtendILangStringDefinitionTypeIec61360Test.cs
@@ -0,0 +1,61 @@
+﻿using AasCore.Aas3_0;
+using aaspe_common.AasxCsharpLibrary.Extensions;
+using AasxCompatibilityModels;
+
+namespace aaspe_common_tests.AasxCsharpLibrary.Extensions;
+
+[TestSubject(typeof(ExtendILangStringDefinitionTypeIec61360))]
+public class ExtendILangStringDefinitionTypeIec61360Test
+{
+    [Fact]
+    public void CreateLangStringDefinitionType_ShouldCreateListWithSingleLangString()
+    {
+        // Arrange
+        const string language = "en";
+        const string text = "Example Text";
+
+        // Act
+        var result = ExtendILangStringDefinitionTypeIec61360.CreateLangStringDefinitionType(language, text);
+
+        // Assert
+        result.Should().HaveCount(1);
+        result.First().Language.Should().Be(language);
+        result.First().Text.Should().Be(text);
+    }
+
+    [Fact]
+    public void GetDefaultString_WhenDefaultLangExists_ShouldReturnTextOfDefaultLang()
+    {
+        // Arrange
+        var langStringSet = new List<ILangStringDefinitionTypeIec61360>
+        {
+            new LangStringDefinitionTypeIec61360("en", "English Text"),
+            new LangStringDefinitionTypeIec61360("de", "German Text")
+        };
+
+        // Act
+        var result = langStringSet.GetDefaultString();
+
+        // Assert
+        result.Should().Be("English Text");
+    }
+
+    [Fact]
+    public void ConvertFromV20_WhenSourceIsNotNull_ShouldConvertToLangStringDefinitionType()
+    {
+        // Arrange
+        var src = new AdminShellV20.LangStringSetIEC61360
+        {
+            new() {lang = "en", str = "English Text"},
+            new() {lang = "de", str = "German Text"}
+        };
+
+        // Act
+        var result = new List<ILangStringDefinitionTypeIec61360>().ConvertFromV20(src);
+
+        // Assert
+        result.Should().HaveCount(2);
+        result.Should().ContainEquivalentOf(new LangStringDefinitionTypeIec61360("en", "English Text"));
+        result.Should().ContainEquivalentOf(new LangStringDefinitionTypeIec61360("de", "German Text"));
+    }
+}
--- src/aaspe-common/AasxCsharpLibrary/Extensions/ExtendILangStringDefinitionTypeIec61360.cs
+++ src/aaspe-common/AasxCsharpLibrary/Extensions/ExtendILangStringDefinitionTypeIec61360.cs
@@ -1,59 +1,61 @@
-﻿/*
-Copyright (c) 2018-2023 Festo SE & Co. KG <https://www.festo.com/net/de_de/Forms/web/contact_international>
-Author: Michael Hoffmeister
-
-This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
-
-This source code may use other Open Source software components (see LICENSE.txt).
-*/
-
-using AasxCompatibilityModels;
+﻿using AasxCompatibilityModels;
 
 namespace aaspe_common.AasxCsharpLibrary.Extensions;
 
+/// <summary>
+/// Provides extension methods for ILangStringDefinitionTypeIec61360.
+/// </summary>
 public static class ExtendILangStringDefinitionTypeIec61360
 {
+    private const string DefaultLanguage = "en";
+
+    /// <summary>
+    /// Creates a list containing a single ILangStringDefinitionTypeIec61360 instance with the specified language and text.
+    /// </summary>
+    /// <param name="language">The language of the lang string.</param>
+    /// <param name="text">The text of the lang string.</param>
+    /// <returns>A list containing a single ILangStringDefinitionTypeIec61360 instance.</returns>
     public static List<ILangStringDefinitionTypeIec61360> CreateLangStringDefinitionType(string language, string text)
     {
-        return new List<ILangStringDefinitionTypeIec61360> { new LangStringDefinitionTypeIec61360(language, text) };
+        return new List<ILangStringDefinitionTypeIec61360> {new LangStringDefinitionTypeIec61360(language, text)};
     }
-    public static string? GetDefaultString(this List<ILangStringDefinitionTypeIec61360> langStringSet, string defaultLang = "en")
+
+    /// <summary>
+    /// Gets the default string from a list of lang strings using the specified default language.
+    /// </summary>
+    /// <param name="langStringSet">The list of lang strings.</param>
+    /// <param name="defaultLang">The default language.</param>
+    /// <returns>The default string.</returns>
+    public static string? GetDefaultString(this List<ILangStringDefinitionTypeIec61360> langStringSet, string defaultLang = DefaultLanguage)
     {
-        string? res = null;
+        var matchingLangString = langStringSet.FirstOrDefault(langString => langString.Language.Equals(defaultLang, StringComparison.OrdinalIgnoreCase));
 
-        // search
-        foreach (var langString in langStringSet.Where(langString => langString.Language.Equals(defaultLang, StringComparison.OrdinalIgnoreCase)))
-            res = langString.Text;
-
-        if (res == null && langStringSet.Count > 0)
-            res = langStringSet[0].Text;
-
-        return res;
+        return matchingLangString?.Text ?? langStringSet.FirstOrDefault()?.Text;
     }
+
+    /// <summary>
+    /// Converts from AdminShellV20.LangStringSetIEC61360 to a list of ILangStringDefinitionTypeIec61360.
+    /// </summary>
+    /// <param name="lss">The list of lang strings to be populated.</param>
+    /// <param name="src">The source lang string set.</param>
+    /// <returns>The converted list of lang strings.</returns>
     public static List<ILangStringDefinitionTypeIec61360> ConvertFromV20(
         this List<ILangStringDefinitionTypeIec61360> lss,
         AdminShellV20.LangStringSetIEC61360? src)
     {
         lss = new List<ILangStringDefinitionTypeIec61360>();
-        if (src != null && src.Count != 0)
+
+        if (src != null && src.Any())
         {
-            foreach (var sourceLangString in src)
-            {
-                //Remove ? in the end added by AdminShellV20, to avoid verification error
-                var lang = sourceLangString.lang;
-                if (!string.IsNullOrEmpty(sourceLangString.lang) && sourceLangString.lang.EndsWith('?'))
-                {
-                    lang = sourceLangString.lang.Remove(sourceLangString.lang.Length - 1);
-                }
-                var langString = new LangStringDefinitionTypeIec61360(lang, sourceLangString.str);
-                lss.Add(langString);
-            }
+            lss.AddRange((src.Select(sourceLangString => new {sourceLangString, lang = sourceLangString.lang?.TrimEnd('?')})
+                .Select(@t => new LangStringDefinitionTypeIec61360(@t.lang ?? DefaultLanguage, @t.sourceLangString.str))));
         }
         else
         {
-            //set default preferred name
-            lss.Add(new LangStringDefinitionTypeIec61360("en", ""));
+            // Set default preferred name
+            lss.Add(new LangStringDefinitionTypeIec61360(DefaultLanguage, string.Empty));
         }
+
         return lss;
     }
 }
